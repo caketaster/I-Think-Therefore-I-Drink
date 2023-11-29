7e3a9a39f534a7cc07a3ac5246246a603bdf0ef8
caketaster <caketaster@gmail.com>
2023-11-29 05:05:44 +0000

    create submit form model for users to submit cocktail recipes

--- recipe/models.py
+++ recipe/models.py
@@ -17,7 +17,7 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     # n.b. favourites not 'likes' (integer?)
     favourites = models.ManyToManyField(User, related_name='recipe_favourites', blank=True)
-    # n.b. ingrdients is a list. not sure how to do this - CharField? required/blank=False
+    # n.b. ingredients is a list. not sure how to do this - CharField? required/blank=False
     ingredients = models.CharField(max_length=800)
     # n.b. instructions is obvs new
     instructions = models.CharField(max_length=800)
@@ -33,6 +33,32 @@ class Post(models.Model):
     # n.b. in the walkthrough this is number_of_likes
     def number_of_favourites(self):
         return self.favourites.count()
+
+
+class submit_form(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    # n.b. recipe_posts not blog_posts
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="submit_posts")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    # n.b. ingredients is a list. not sure how to do this - CharField? required/blank=False
+    ingredients = models.CharField(max_length=800)
+    # n.b. instructions is obvs new
+    instructions = models.CharField(max_length=800)
+
+    class Meta:
+        # in descending order by created on - might change this ordering
+        ordering = ['-created_on']
+
+    # a 'magic method' that returns a string representation of an object. You should define it because the default isn't helpful at all(!)
+    def __str__(self):
+        return self.title
+
     
 # # I'm not having comments, I'm having recipe submissions. Will comment this out until I can create the submission form and replace it
 # class Comment(models.Model):
